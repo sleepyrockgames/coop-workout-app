fdc0ed78ebe2cad9d9bc6990eecc0d01fa65f344
eruhlinteractive <epruhl102@gmail.com>
2025-10-17 18:57:27 -0400

    Fixed stuff

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// TODO: This doesnt work for some reason??
-//import vueDevTools from 'vite-plugin-vue-devtools'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    //vueDevTools(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
